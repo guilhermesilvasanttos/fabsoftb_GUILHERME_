1b4e83e1fb130ebc85d6da878cb2ae31a312b48c
GUILHERME MIRANDA SILVA DOS SANTOS <guilherme.miranda@univille.br>
2025-03-11 18:52:21 -0300

    revisao

---
+++ revisao_guilherme/src/main/java/br/univille/Main.java
@@ -0,0 +1,16 @@
+package br.univille;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+
+        cliente zezinho = new cliente();
+        zezinho.setNome("zezinho sila");
+        //variável -> é driada e moldada da forma desejada
+        //new -> comando para criar instancia
+        //Sinal = -> reia um referência para o objeto
+        //tipo primitivo - ex int idade = 20
+        cliente mariazinha = new cliente();
+        mariazinha.setNome("Mariazinha");
+    }
+}
